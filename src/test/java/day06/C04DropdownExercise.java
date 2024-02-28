29cdff67792f50c5466d76f4d8a2a591d4d96330
ReemaEbbie <reema.ebbie@gmail.com>
2024-02-29 01:25:38 +0300

    reema

---
+++ src/test/java/day06/C04DropdownExercise.java
@@ -0,0 +1,44 @@
+package day06;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+public class C04DropdownExercise {
+    /*
+    https://demoqa.com/select-menu
+    Get all the options of the dropdown
+    Options size
+    Print all test
+    Verify the dropdown has option "Black"
+    Print FirstSelectedOptionTest
+    Select option "Yellow"
+     */
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/select-menu");
+    }
+    @AfterClass
+    public static void tearDown (){
+        //driver.quit();
+    }
+    @Test
+    public void test(){
+        //Get all the options of the dropdown
+        //Options size
+        //Print all test
+        //Verify the dropdown has option "Black"
+        //Print FirstSelectedOptionTest
+        //Select option "Yellow"
+
+
+
+
+    }
+}
